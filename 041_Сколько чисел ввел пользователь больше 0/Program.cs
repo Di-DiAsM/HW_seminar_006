c25e6ce34ad4431a10548b3b211ab69aa5059f8c
Di <dimas2343@mail.ru>
2022-08-29 22:23:12 +0300

    infinite palindrom

---
+++ 041_Сколько чисел ввел пользователь больше 0/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+Console.Clear();
+
+Console.Write("Сколько чисел вы будете вводить: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] numbers = new int[size];
+
+int count = 0;
+for (int i = 0; i < size; i++)
+{
+    Console.Write("Введите число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    numbers[i] = num;
+    if (numbers[i] > 0) count++;
+}
+
+Console.Write(string.Join(" ", numbers));
+Console.Write($" - введено чисел больше 0: {count}");
+
+
---
+++ 043_точка пересечения двух прямых/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// y =  a * x + c
+// y = k1 * x + b1  **
+
+// y =  b * x + d
+// y = k2 * x + b2  **
+
+// x = (d - c) / a - b
+// x = b2 - b1 / k1 - k2  **
+
+// y = a * (d - c / a - b) + c
+// y = k1 * x + c **
+
+// if a == b - две прямые параллельны
+//   k1 == k2  **
+// с  != d - пряме различны и не имеют пересений, в протовном случае прямые совпадают
+// b1 != b2 ** 
+
+Console.Clear();
+
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("для первого уравнения y1 = k1 * x + b1");
+double b1 = ReadInt("введите b1: ");
+double k1 = ReadInt("введите k1: ");
+
+
+Console.WriteLine("для второго уравнения y2 = k2 * x + b2");
+double b2 = ReadInt("введите b2: ");
+double k2 = ReadInt("введите k2: ");
+
+if (k1 == k2 && b1 == b2)
+{
+    Console.WriteLine("Прямые сопадают и имеют бесконечное количество общих точек");
+}
+else if (k1 == k2)
+{
+    Console.WriteLine("Две прямые параллельны");
+}
+else
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Точка пересечения прямых: ({x}, {y})");
+}
+
